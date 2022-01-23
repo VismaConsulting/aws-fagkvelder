89bff0f0fdcba05cf8c97886fd136ed3c0734cf2
Eira Mørch-Thoresen <eiramth@hotmail.com>
2022-01-23 11:06:01 +0100

    fikset automatisk deployment av API i cloudformation

--- 25.01.2022/SAM/commands.sh
+++ 25.01.2022/SAM/commands.sh
@@ -2,7 +2,7 @@
 aws s3 mb s3://PREFIX-sam-deployment
 
 # package code
-aws cloudformation package --s3-bucket PREFIX-sam-deployment --template-file template-sertifiseringer.yaml --output-template-file gen/template-sertifiseringer-generated.yaml
+aws cloudformation package --s3-bucket PREFIX-sam-deployment --template-file sam-template-sertifiseringer.yaml --output-template-file gen/template-sertifiseringer-generated.yaml
 
 # deploy
 aws cloudformation deploy --template-file gen/template-sertifiseringer-generated.yaml --stack-name PREFIX-sam-stack --capabilities CAPABILITY_IAM
